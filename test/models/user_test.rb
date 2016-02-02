f95a116b3983c5c4358a389677deac5fe06025ab
Francisco Oliveira <fas.oliveira@gmail.com>
2016-02-02 18:39:28 +0000

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,4 +72,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+  
+  test "should follow and unfollow a user" do
+    francisco = users(:francisco)
+    archer    = users(:archer)
+    assert_not francisco.following?(archer)
+    francisco.follow(archer)
+    assert francisco.following?(archer)
+    assert archer.followers.include?(francisco)
+    francisco.unfollow(archer)
+    assert_not francisco.following?(archer)
+  end
+  
+  test "feed should have the right posts" do
+    francisco = users(:francisco)
+    archer    = users(:archer)
+    lana      = users(:lana)
+    # Posts from following users
+    lana.microposts.each do |post_following|
+      assert francisco.feed.include?(post_following)
+    end
+    # Posts from self
+    francisco.microposts.each do |post_self|
+      assert francisco.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not francisco.feed.include?(post_unfollowed)
+    end
+  end
 end
